fa2b33d0e6668d10d4398efd28b1f237cb2cc093
devengturner <devengturner93@gmail.com>
2024-01-26 20:03:00 -0500

    Add starter html, css, and js

---
+++ script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(sign, a, b) {
+  switch (sign) {
+    case "+":
+      return add(a, b);
+
+    case "-":
+      return subtract(a, b);
+
+    case "*":
+      return multiply(a, b);
+
+    case "/":
+      return divide(a, b);
+
+    default:
+      return null;
+  }
+}
+
+console.log(operate("*", -5, 1));
